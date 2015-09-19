bfb5b92bbdebe28e26be95d20cd5d6fe0c771d7a
celio <celio@delogic.com.br>
2015-09-19 16:44:52 -0300

    Create new ASD version without utilities

--- asd-core/src/main/java/br/com/delogic/asd/util/Convertable.java
+++
@@ -1,7 +0,0 @@
-package br.com.delogic.asd.util;
-
-public interface Convertable<IN, OUT> {
-
-    OUT convert(IN in);
-
-}
--- asd-core/src/main/java/br/com/delogic/asd/util/Each.java
+++
@@ -1,7 +0,0 @@
-package br.com.delogic.asd.util;
-
-public interface Each<E> {
-
-    void each(E e, int index);
-
-}
